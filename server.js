af9e69da2c25fc20af1105f22211d28e0bd31500
thelordaleks <thelordaleks@gmail.com>
2025-10-19 19:36:21 +0200

    test wallet-model log

--- server.js
+++ server.js
@@ -261,6 +261,10 @@ app.get('/wallet/:code', async (req, res) => {
 
 // Lancement du serveur
 initDb().then(() => {
+  console.log("== Wallet model contents ==");
+console.log(fs.readdirSync(path.join(process.cwd(), "wallet-model")));
+console.log("pass.json exists:", fs.existsSync(path.join(process.cwd(), "wallet-model", "pass.json")));
+
   app.listen(PORT, () => console.log('Listening on', PORT));
 }).catch(e => {
   console.error('DB init failed:', e); process.exit(1);
